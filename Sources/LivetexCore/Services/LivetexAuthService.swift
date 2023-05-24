4369be9e7b358db78b7214dbf520affece70ac21
Iurii Kotikhin <kotikhinyv@gmail.com>
2023-05-25 07:27:29 +1000

    изменен инициализатор, добавлен алгоритм выбора

--- Sources/LivetexCore/Services/LivetexAuthService.swift
+++ Sources/LivetexCore/Services/LivetexAuthService.swift
@@ -8,27 +8,35 @@
 
 import Foundation
 
-public enum Token {
-    case system(String)
-    case custom(String)
+//public enum Token {
+//    case system(String)
+//    case custom(String)
+//}
+
+public struct AuthData {
+    let customVisitorToken: String?
+    let visitorToken: String?
 }
 
 private let defaultAuthPath = "https://visitor-api.livetex.ru/v1/auth"
 
 public class LivetexAuthService {
 
-    private let token: Token?
+  //  private let token: Token?
 
     private let deviceToken: String
-
     private let authPath: String?
+    private let visitorToken: String?
+    private let customVisitorToken: String?
 
     // MARK: - Initialization
 
-    public init(token: Token? = nil,
+    public init(visitorToken: String? = nil,
+                customVisitorToken: String? = nil,
                 deviceToken: String,
                 authPath: String? = nil) {
-        self.token = token
+        self.visitorToken = visitorToken
+        self.customVisitorToken = customVisitorToken
         self.deviceToken = deviceToken
         self.authPath = authPath
     }
@@ -44,21 +52,17 @@ public class LivetexAuthService {
             URLQueryItem(name: "deviceType", value: "ios")
         ].filter { $0.value != nil }
 
-        if let token = token {
-            switch token {
-            case let .system(value):
-                UserDefaults.standard.set(value, forKey: "visitorToken")
-                components?.queryItems?.append(URLQueryItem(name: "visitorToken", value: value))
-            case let .custom(value):
-                let visitorToken = UserDefaults.standard.string(forKey: "visitorToken")
 
-                if let visitorToken = visitorToken {
-                    components?.queryItems?.append(URLQueryItem(name: "visitorToken", value: visitorToken))
-                    components?.queryItems?.append(URLQueryItem(name: "customVisitorToken", value: value))
-                } else {
-                    components?.queryItems?.append(URLQueryItem(name: "customVisitorToken", value: value))
-                }
-            }
+        switch (visitorToken, customVisitorToken) {
+        case (visitorToken, customVisitorToken):
+            components?.queryItems?.append(URLQueryItem(name: "visitorToken", value: visitorToken))
+            components?.queryItems?.append(URLQueryItem(name: "customVisitorToken", value: customVisitorToken))
+        case (visitorToken, nil):
+            components?.queryItems?.append(URLQueryItem(name: "visitorToken", value: visitorToken))
+        case (nil, customVisitorToken):
+            components?.queryItems?.append(URLQueryItem(name: "customVisitorToken", value: customVisitorToken))
+        default:
+            components?.queryItems?.append(URLQueryItem(name: "visitorToken", value: ""))
         }
 
         guard let authURL = components?.url else {
